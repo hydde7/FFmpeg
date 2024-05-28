1b42f5c509762679ba0f42b6bd6c1ac41e597e36
Eddy Santos <guto_eddy@hotmail.com>
2024-05-28 17:29:15 -0300

    (feat): Change hls_read_packet function to set the filename of AVPacket to last_url

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -2296,12 +2296,19 @@ static int hls_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
     HLSContext *c = s->priv_data;
     int ret, i, minplaylist = -1;
+    char *last_url = NULL;
 
     recheck_discard_flags(s, c->first_packet);
     c->first_packet = 0;
 
     for (i = 0; i < c->n_playlists; i++) {
         struct playlist *pls = c->playlists[i];
+        struct segment *auxSeg = NULL;
+
+        auxSeg = current_segment(pls);
+        if (auxSeg != NULL && auxSeg->url != NULL){
+            last_url = strdup(auxSeg->url);
+        }
         /* Make sure we've got one buffered packet from each open playlist
          * stream */
         if (pls->needed && !pls->pkt->data) {
@@ -2432,6 +2439,13 @@ static int hls_read_packet(AVFormatContext *s, AVPacket *pkt)
             }
         }
 
+        if (last_url != NULL) {
+            if (pkt->filename != NULL) {
+                free(pkt->filename);
+            }
+            pkt->filename = last_url;
+            last_url = NULL;
+        }
         return 0;
     }
     return AVERROR_EOF;
